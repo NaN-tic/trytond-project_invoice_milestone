e7b556920f61f183310d041cd0cc59a89aa396dc
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-08-16 10:27:11 +0200

    Upgrade test

--- tests/test_project_invoice_milestone.py
+++ tests/test_project_invoice_milestone.py
@@ -5,6 +5,7 @@ import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
 class TestCase(ModuleTestCase):
@@ -14,9 +15,10 @@ class TestCase(ModuleTestCase):
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    # TODO: suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
     suite.addTests(doctest.DocFileSuite(
             'scenario_project_invoice_milestone_manual.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
